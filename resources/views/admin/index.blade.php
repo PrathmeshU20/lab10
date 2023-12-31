c3412fa27d8e990241d110fdcf328dc05b48b15f
PrathmeshU20 <prathmeshurkude20@gmail.com>
2023-12-31 23:34:21 +0530

    Add tailwindcss typography and markdown html conversion

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,6 +5,10 @@
     <x-slot name="title">
         {{ __('Admin Section')}}
     </x-slot>
-
-    This is the content of my admin section.
+    This is the content of my admin home.
+    @if(isset($html))
+    <div class="p-2 prose">
+    {!! $html !!}
+    </div>
+    @endif
 </x-admin-layout>
